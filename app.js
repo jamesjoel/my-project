42c7bd6445f86bda6817c174ad027ce93d7b471f
jamesjoel <jamesjoelj@gmail.com>
2023-06-26 16:01:28 +0530

    assets folder

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname+"/assets"));
 
 app.get("/", (req, res)=>{
-    res.sendFile(__+"/navbar.html");
+    res.sendFile(__dirname+"/navbar.html");
 })
 
 const port  = process.env.PORT || 3000;
